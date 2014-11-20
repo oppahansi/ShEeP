9ee4958aef0117df84846bba2113dead653854ef
d4k3 <dakenkrowe@googlemail.de>
2014-11-20 21:33:50 +0100

    added methods at entities

--- src/de/sepab/sheep/model/entities/Player.java
+++ src/de/sepab/sheep/model/entities/Player.java
@@ -4,4 +4,34 @@ package de.sepab.sheep.model.entities;
  * Created by oppahansi on 18.11.2014.
  */
 public class Player extends Entity{
+
+	@Override
+	public int getPosX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPosX() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getPosY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPosY() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void event() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/de/sepab/sheep/model/entities/Sheep.java
+++ src/de/sepab/sheep/model/entities/Sheep.java
@@ -2,4 +2,34 @@ package de.sepab.sheep.model.entities;
 
 public class Sheep extends Entity{
 
+	@Override
+	public int getPosX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPosX() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getPosY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPosY() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void event() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
