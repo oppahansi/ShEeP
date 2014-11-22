cc599a68a2e30c1905ef45d6acbada6ad43a9e9b
d4k3 <dakenkrowe@googlemail.de>
2014-11-22 17:26:00 +0100

    Timer-Klasse

--- src/de/sepab/sheep/model/corelogic/ITimer.java
+++ src/de/sepab/sheep/model/corelogic/ITimer.java
@@ -1,4 +1,8 @@
 package de.sepab.sheep.model.corelogic;
 
 public interface ITimer {
+	public void reset();
+	public void start();
+	public void stop();
+	public int getTime();
 }
--- src/de/sepab/sheep/model/corelogic/Timer.java
+++ src/de/sepab/sheep/model/corelogic/Timer.java
@@ -1,4 +1,47 @@
 package de.sepab.sheep.model.corelogic;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Timer  implements ITimer{
+
+	private Date StartTime;
+	private Date StopTime;
+	private SimpleDateFormat FormatSec = new SimpleDateFormat("ss");
+	private SimpleDateFormat FormatMin = new SimpleDateFormat("mm");
+	
+	public Timer() {
+		this.reset();
+	}
+	
+	@Override
+	public void reset() {
+		this.StartTime = new Date();
+		this.StopTime = this.StartTime;
+	}
+
+	@Override
+	public void start() {
+		this.StartTime = new Date();
+	}
+
+	@Override
+	public void stop() {
+		this.StopTime = new Date(); 
+	}
+
+	@Override
+	public int getTime() {
+		return ((
+				(60*Integer.parseInt(this.FormatMin.format(this.StopTime)))
+				+
+				(Integer.parseInt(this.FormatSec.format(this.StopTime)))
+				)
+				-
+				(
+				(60*Integer.parseInt(this.FormatMin.format(this.StartTime)))
+				+
+				(Integer.parseInt(this.FormatSec.format(this.StartTime)))
+				));
+	}
 }
