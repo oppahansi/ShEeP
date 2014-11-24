1104c76b4f7d57ccacf2a9e0a7ffa38aca21254a
d4k3 <dakenkrowe@googlemail.de>
2014-11-24 17:05:04 +0100

    alt-lasten

--- src/de/sepab/sheep/view/game/GameBoard.java
+++ src/de/sepab/sheep/view/game/GameBoard.java
@@ -1,9 +1,4 @@
 package de.sepab.sheep.view.game;
 
-import de.sepab.sheep.model.corelogic.IPowerUpCreator;
-import de.sepab.sheep.model.entities.IEntity;
-import de.sepab.sheep.model.entities.Player;
-
 public class GameBoard implements IGameBoard{
-    private IPowerUpCreator powerUpCreator;
 }
