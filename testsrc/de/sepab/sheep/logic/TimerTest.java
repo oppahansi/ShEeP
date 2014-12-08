cee52f6271c2371a7cb146fbd05b20c78276a644
McMario <mario-schwanke@gmx.de>
2014-12-08 15:16:01 +0100

    TimerTest hinzugefügt.

--- testsrc/de/sepab/sheep/logic/TimerTest.java
+++ testsrc/de/sepab/sheep/logic/TimerTest.java
@@ -1,14 +1,31 @@
 package de.sepab.sheep.logic;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import de.sepab.sheep.logic.Timer;
 
-public class TimerTest {
-
+public class TimerTest extends Thread {
+	
+	Timer tester = new Timer();
+	
+	public void run() {
+	    tester.start(); 
+		try {
+	    	 sleep(20000);
+	     }
+	     catch(InterruptedException e) {
+	     }
+	    tester.stop();
+		System.out.println(tester.getTime());
+	     
+	    }
+	
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void TimerTest() {
+		TimerTest t = new TimerTest();
+		t.run();
 	}
 
 }
