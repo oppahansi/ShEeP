87acde0e9ff65ba369ed40a51a3ff1c9db335441
Bathyl <maxmueller53@gmx.de>
2014-12-08 17:56:02 +0100

    test

--- src/de/sepab/sheep/logic/Collision.java
+++ src/de/sepab/sheep/logic/Collision.java
@@ -1,11 +1,48 @@
 package de.sepab.sheep.logic;
 
-import de.sepab.sheep.entities.Entity;
+import de.sepab.sheep.entities.IEntity;
+import de.sepab.sheep.entities.ILevel;
 
 public class Collision {
 	public boolean calCollision(Entity entity, int x, int y){
 		//Wäre echt nicer wenn das ne Entity zurüch gibt.
 		return false;
 	}
-	
+	puplic boolean isoccupied(int x,int y)
+	{
+		//getDogList();getSheepList();getPowerUpList();getObstacleList()
+		/*
+		 * if(0<x<1281 && 0<y<961)
+		 * {
+		 	liste mit allen entitys finden
+		 	for(int i=0;i<getDogList().size;i++)
+		 	{
+				int Y[2];
+				int X[2];
+				x[0]=entity.getX()-32;
+				x[1]=entity.getX()+32;
+				y[0]=entity.getY()-32;
+				y[1]=entity.getY()+32;
+
+			if(x[0]<List[i].getX()<x[1] && y[0]<list[i].getY()<y[1])
+			{//COLLISION
+			return false;}
+		 	}
+		 	else{return true;}
+		 	}
+		 	}
+		 	return false;
+		*/
+	}
 }
+
+
+/*
+Sprite auflösung abfragen?kann ich das? - nein muss hardegecodet werden
+sind die koordinaten der entity links unten vom bild? oder z.b. zentral mitte? - links oben
+hunde sind 64x64
+schafe sind 64x64
+hindernis immer 32x32?
+ALLES IST 32x32
+
+*/
--- src/de/sepab/sheep/logic/Movement.java
+++ src/de/sepab/sheep/logic/Movement.java
@@ -1,12 +1,34 @@
 package de.sepab.sheep.logic;
-
+//feld=1280x960
 import de.sepab.sheep.entities.IEntity;
 
 public class Movement implements IMovement {
-
+	//1=hoch,2=rechts,3=runter,4=links,0=nix
 	@Override
 	public void move(IEntity entity, int direction) {
 		// TODO Auto-generated method stub
+			
+			switch(direction)
+				case(0)
+					if(collision.isoccupied(entiry.getX(),entity.getY()))
+					{}
+					break;
+				case(1)
+					if(collision.isoccupied(entity.getX(),entity.getY()+1))
+					{entity.setY(entity.getY+1);}
+					break;
+				case(2)
+					if(collision.isoccupied(entity.getX()+1,entity.getY()))
+					{entity.setX(entity.getX+1);}
+					break;
+				case(3)
+					if(collision.isoccupied(entity.getX(),entity.getY()-1))
+					{entity.setY(entity.getY-1);}
+					break;
+				case(4)
+					if(collision.isoccupied(entity.getX()-1,entity.getY()))
+					{entity.setX(entity.getX()-1);}
+					break;
 		
 	}
 
