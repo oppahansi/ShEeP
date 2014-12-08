6bd20e9cd1526c14f50a29f0c44fa218e6fa800d
d4k3 <dakenkrowe@googlemail.de>
2014-12-08 17:10:12 +0100

    sollte jetzt funktionieren

--- src/de/sepab/sheep/handler/AI.java
+++ src/de/sepab/sheep/handler/AI.java
@@ -11,6 +11,14 @@ import de.sepab.sheep.logic.RandomGenerator;
 
 public class AI {
 	
+	/*
+	 * Mitte:	0
+	 * Oben:	1
+	 * Rechts:	2
+	 * Unten:	3
+	 * Links:	4
+	 */
+	
 	private int iq;
 	private int loss;
 	private LinkedList<IEntity> sheeps;
@@ -35,17 +43,87 @@ public class AI {
 	
 	private int calcNextStep(IEntity entity) {
 		ISheep sheep = (ISheep)entity;
-		int nextStep = this.snityCheck(sheep.getThoughts());
+		int nextStep = this.sanityCheck(entity);
 		this.checkPath(sheep.getThoughts());
 		return nextStep;
 	}
 	
-	private int snityCheck(int[] thoughts) {
-		return 0;
+	private int sanityCheck(IEntity sheep) {
+		boolean[] possibleSteps = {false, false, false, false};
+		int[] realNextSteps;
+		int n = 0;
+		int step = 0;
+		int tmpX = 0, tmpY = 0;
+		switch(((ISheep)sheep).getThoughts()[0]) {
+		case (0):
+			break;
+		case (1):
+			tmpY=1;
+			break;
+		case (2):
+			tmpX=1;
+			break;
+		case (3):
+			tmpY=-1;
+			break;
+		case (4):
+			tmpX=-1;
+			break;
+		}
+		if(!this.collisionHandler.calcCollision(sheep, sheep.getX()+tmpX, sheep.getY()+tmpY)) {
+			step = ((ISheep)sheep).getThoughts()[0];
+		}
+		else {
+			if(!this.collisionHandler.calcCollision(sheep, sheep.getX(), sheep.getY()+1)) possibleSteps[0]=true;
+			if(!this.collisionHandler.calcCollision(sheep, sheep.getX()+1, sheep.getY())) possibleSteps[1]=true;
+			if(!this.collisionHandler.calcCollision(sheep, sheep.getX(), sheep.getY()-1)) possibleSteps[2]=true;
+			if(!this.collisionHandler.calcCollision(sheep, sheep.getX()-1, sheep.getY())) possibleSteps[3]=true;
+			for(int i=0; i<possibleSteps.length;i++) {
+				if(possibleSteps[i]) n++;
+			}
+			realNextSteps = new int[n];
+			n=0;
+			for(int i=0; i<possibleSteps.length; i++) {
+				if(possibleSteps[i]) {
+					realNextSteps[n]=i;
+					n++;
+				}
+			}
+			if(realNextSteps.length == 0) step=0;
+			else {
+				step=realNextSteps[randomGenerator.getRandomNumber(0, (realNextSteps.length-1))];
+			}
+		}
+		if(((ISheep)sheep).isScared()) {
+			int chance = this.randomGenerator.getRandomNumberDistrubution(this.iq, this.loss);
+			if(!(chance > -this.loss && chance < this.loss)) {
+				step=0;
+			}
+		}
+		return step;
 	}
 	
 	private void checkPath(int[] thoughts) {
-		
+		if(thoughts[4] == 0) {
+			int chance = this.randomGenerator.getRandomNumberDistrubution(this.iq, this.loss);
+			if(!(chance > -this.loss && chance < this.loss)) {
+				for(int i=0; i<5; i++) {
+					thoughts[i] = this.randomGenerator.getRandomNumber(1, 4);
+				}
+			}
+			else {
+				int tmp = this.randomGenerator.getRandomNumber(1, 4);
+				for(int i=0; i<4; i++) {
+					thoughts[i] = tmp;
+				}
+			}
+		}
+		else {
+			for(int i=0; i<4; i++) {
+				thoughts[i] = thoughts[i+1];
+			}
+			thoughts[4] = thoughts[this.randomGenerator.getRandomNumber(0, 3)];
+		}
 	}
 	
 }
