0d580b9720a9186091e44d538ebd71817ac48740
Bathyl <maxmueller53@gmx.de>
2014-12-09 17:40:03 +0100

    test5

--- src/de/sepab/sheep/logic/Movement.java
+++ src/de/sepab/sheep/logic/Movement.java
@@ -4,17 +4,23 @@ import de.sepab.sheep.entities.IEntity;
 import de.sepab.sheep.logic.ICollision;
 
 public class Movement implements IMovement {
-boolean ichbinfaulichbindoll;
-	@Override
+
+	
+private ICollision collision;
+	
+	public Movement (ICollision collisionHandler)
+	{
+		collision=collisionHandler;
+	}
 	public void move(IEntity entity, int direction) {
 		//0=nix,1=hoch,2=rechts,3=runter,4=links,
 			switch(direction){
 				case(0):
-					if(Collision.isoccupied(entity.getPosX(),entity.getPosY()))
+					if(collision.isoccupied(entity.getPosX(),entity.getPosY()))
 					{}
 					break;
 				case(1):
-					if(Collision.isoccupied(entity.getPosX(),entity.getPosY()+1))
+					if(collision.isoccupied(entity.getPosX(),entity.getPosY()+1))
 					{entity.setPosY(entity.getPosY()+1);}
 					break;
 				case(2):
