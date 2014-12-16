b90b6e4df7ee2ba2bd61dcbc8ee41b565a568988
Pudding00 <poddig.tim@web.de>
2014-12-16 16:33:25 +0100

    neuster commit

--- src/de/sepab/sheep/entities/Dog.java
+++ src/de/sepab/sheep/entities/Dog.java
@@ -80,58 +80,58 @@ public class Dog extends Entity implements IDog {
 		case 0:
 			break;
 		case 1:
-			if (spriteCount >= 0 && spriteCount < 4) {
+			if (spriteCount >= 0 && spriteCount < 8) {
 				this.setSpritePos(4);
 			}
-			if (spriteCount >= 4 && spriteCount < 8) {
+			if (spriteCount >= 8 && spriteCount < 16) {
 				this.setSpritePos(5);
 			}
-			if (spriteCount >= 8 && spriteCount < 12) {
+			if (spriteCount >= 16 && spriteCount < 24) {
 				this.setSpritePos(6);
 			}
-			if (spriteCount >= 12 && spriteCount < 16) {
+			if (spriteCount >= 24 && spriteCount < 32) {
 				this.setSpritePos(7);
 			}
 			break;
 		case 2:
-			if (spriteCount >= 0 && spriteCount < 4) {
+			if (spriteCount >= 0 && spriteCount < 8) {
 				this.setSpritePos(8);
 			}
-			if (spriteCount >= 4 && spriteCount < 8) {
+			if (spriteCount >= 8 && spriteCount < 16) {
 				this.setSpritePos(9);
 			}
-			if (spriteCount >= 8 && spriteCount < 12) {
+			if (spriteCount >= 16 && spriteCount < 24) {
 				this.setSpritePos(10);
 			}
-			if (spriteCount >= 12 && spriteCount < 16) {
+			if (spriteCount >= 24 && spriteCount < 32) {
 				this.setSpritePos(11);
 			}
 			break;
 		case 3:
-			if (spriteCount >= 0 && spriteCount < 4) {
+			if (spriteCount >= 0 && spriteCount < 8) {
 				this.setSpritePos(12);
 			}
-			if (spriteCount >= 4 && spriteCount < 8) {
+			if (spriteCount >= 8 && spriteCount < 16) {
 				this.setSpritePos(13);
 			}
-			if (spriteCount >= 8 && spriteCount < 12) {
+			if (spriteCount >= 16 && spriteCount < 24) {
 				this.setSpritePos(14);
 			}
-			if (spriteCount >= 12 && spriteCount < 16) {
+			if (spriteCount >= 24 && spriteCount < 32) {
 				this.setSpritePos(15);
 			}
 			break;
 		case 4:
-			if (spriteCount >= 0 && spriteCount < 4) {
+			if (spriteCount >= 0 && spriteCount < 8) {
 				this.setSpritePos(0);
 			}
-			if (spriteCount >= 4 && spriteCount < 8) {
+			if (spriteCount >= 8 && spriteCount < 16) {
 				this.setSpritePos(1);
 			}
-			if (spriteCount >= 8 && spriteCount < 12) {
+			if (spriteCount >= 16 && spriteCount < 24) {
 				this.setSpritePos(2);
 			}
-			if (spriteCount >= 12 && spriteCount < 16) {
+			if (spriteCount >= 24 && spriteCount < 32) {
 				this.setSpritePos(3);
 			}
 			break;
