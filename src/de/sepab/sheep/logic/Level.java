eea61c8a6af9734d24d2d40840c9003df4485d9c
Pudding00 <poddig.tim@web.de>
2015-01-12 11:51:17 +0100

    bla

--- src/de/sepab/sheep/logic/Level.java
+++ src/de/sepab/sheep/logic/Level.java
@@ -154,18 +154,19 @@ public class Level implements ILevel, ActionListener{
 													 this.cageList.getFirst().getPosY(),
 													 ((ICage) this.cageList.getFirst()).getPosX2(),
 													 ((ICage)this.cageList.getFirst()).getPosY2()));
-		float i = timer.getTime();	
-		if ((i/2- (timer.getTime()/2) == 0)) {
-			if (spwaned == false) {
-				this.addPowerUp();
-				spwaned = true;
-			}else{
-				spwaned = true;
-			}	
-		}else{
-			spwaned = false;
-		}
-		
+//		float i = timer.getTime();	
+//		if ((i/2- (timer.getTime()/2) == 0)) {
+//			if (spwaned == false) {
+//				this.addPowerUp();
+//				spwaned = true;
+//			}else{
+//				spwaned = true;
+//			}	
+//		}else{
+//			spwaned = false;
+//		}
+//		
+		addPowerUp();
 		switch (gameModus) {
 		case ONTIME:
 			if ((timer.getTime() - time) >= 0) {
